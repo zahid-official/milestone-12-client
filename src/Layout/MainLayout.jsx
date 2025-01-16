c0ab3a4d7423b06daa72a21bf6b742b5f7eeb1fd
zahid-official <zahid.official8@gmail.com>
2025-01-16 14:42:32 +0600

    Navbar Footer Auth

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+import Footer from "../Shared/Footer";
+
+const MainLayout = () => {
+  const location = useLocation();
+  return (
+    <div className="duration-500 dark:bg-[#010313]  dark:text-white">
+      <header>
+        {location.pathname === '/' || <Navbar></Navbar>}
+      </header>
+
+      <main className="min-h-[45vh]">
+        <Outlet></Outlet>
+      </main>
+
+      <footer className="bg-[#f9f9f9] dark:bg-[#0a1020]">
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../Shared/Navbar";
+
+const Home = () => {
+    return (
+        <div className="">
+            {/* navbar */}
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
